11cbafdb8e53db5620b8f23e7de0ca0cae7b03d1
Juliane Cassidy <julianecassidy@gmail.com>
2023-04-11 17:47:06 -0600

    add shipmentSchema

--- routes/shipments.js
+++ routes/shipments.js
@@ -3,10 +3,12 @@
 const express = require("express");
 const { BadRequestError } = require("../expressError");
 const router = new express.Router();
+const jsonschema = require("jsonschema");
+const shipmentSchema = require("../shipmentSchema.json")
 
 const { shipProduct } = require("../shipItApi");
 
-/** POST /ship
+/** POST /shipments
  *
  * VShips an order coming from json body:
  *   { productId, name, addr, zip }
@@ -19,6 +21,13 @@ router.post("/", async function (req, res, next) {
     throw new BadRequestError();
   }
   const { productId, name, addr, zip } = req.body;
+
+  const result = jsonschema.validate(
+    req.body, shipmentSchema, { required: true });
+  if (!result.valid) {
+    const errs = result.errors.map(err => err.stack);
+    throw new BadRequestError(errs);
+  }
   const shipId = await shipProduct({ productId, name, addr, zip });
   return res.json({ shipped: shipId });
 });
--- routes/shipments.test.js
+++ routes/shipments.test.js
@@ -22,4 +22,27 @@ describe("POST /", function () {
       .send();
     expect(resp.statusCode).toEqual(400);
   });
+
+  test("throws error if invalid input", async function () {
+    const resp = await request(app)
+      .post("/shipments")
+      .send({
+        productId: "invalid-id",
+        name: 1000,
+        zip: "34892578927308470",
+        shippingDiscount: 100
+      });
+
+      expect(resp.statusCode).toEqual(400);
+      expect(resp.body.error.message)
+        .toContain("instance.productId is not of a type(s) number");
+      expect(resp.body.error.message)
+        .toContain("instance.name is not of a type(s) string");
+      expect(resp.body.error.message)
+        .toContain("instance.zip does not meet maximum length of 10");
+      expect(resp.body.error.message)
+        .toContain(`instance is not allowed to have the additional property \"shippingDiscount\"`);
+      expect(resp.body.error.message)
+        .toContain(`instance requires property \"addr\"`);
+  })
 });
