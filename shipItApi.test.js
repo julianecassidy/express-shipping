c1bc013b46f85b5ae988f17810b4c48f1e895a4a
Alyssa Denary <alyssa.denary@gmail.com>
2023-04-11 21:03:55 -0400

    add mocking axios test

--- shipItApi.test.js
+++ shipItApi.test.js
@@ -1,11 +1,24 @@
 "use strict";
 
+const AxiosMockAdapter = require("axios-mock-adapter");
+const axios = require("axios");
+const axiosMock = new AxiosMockAdapter(axios);
+
+const {SHIPIT_SHIP_URL, SHIPIT_API_KEY }= require("./config");
 const {
   shipProduct,
 } = require("./shipItApi");
 
 
 test("shipProduct", async function () {
+
+  axiosMock.onPost(SHIPIT_SHIP_URL).reply(200, {
+      receipt: {
+        shipId: 2000
+      }
+    }
+  );
+
   const shipId = await shipProduct({
     productId: 1000,
     name: "Test Tester",
@@ -13,5 +26,5 @@ test("shipProduct", async function () {
     zip: "12345-6789",
   });
 
-  expect(shipId).toEqual(expect.any(Number));
+  expect(shipId).toEqual(2000);
 });
